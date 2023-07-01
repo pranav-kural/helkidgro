09066fd22895058c13d03230327cb0ba55081d79
pranav-kural <pranavkural042@gmail.com>
2023-07-01 18:22:34 -0400

    added lanuage support for how it works

--- src/components/home/HowItWorks.js
+++ src/components/home/HowItWorks.js
@@ -1,9 +1,67 @@
-function HomeItWorks() {
+import how_it_works_data from "../../resources/how-it-works_data";
+
+export default function HowItWorks() {
+  const lang = "en";
   return (
-    <div className="App">
-      <h1>HowItWorks</h1>
+    <div className="container my-5">
+      <h1 className="text-center border-bottom text-color-primary text-style-nunito">
+        How it works?
+      </h1>
+      <div className="d-flex flex-column mt-4 px-sm-0 px-lg-5">
+        <div className="row mb-5 align-items-center">
+          <div className="col-sm-12 col-md-8">
+            <p>
+              {lang === "en"
+                ? how_it_works_data.how_it_works_en.section_heading_first
+                : how_it_works_data.how_it_works_fr.section_heading_first}
+            </p>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <img
+              src="./assets/image_sample.png"
+              width={220}
+              height={150}
+              alt="config"
+            />
+          </div>
+        </div>
+
+        <div className="row mb-5 align-items-center">
+          <div className="col-sm-12 col-md-4">
+            <img
+              src="./assets/image_sample.png"
+              width={220}
+              height={150}
+              alt="config"
+            />
+          </div>
+          <div className="col-sm-12 col-md-8">
+            <p>
+              {lang === "en"
+                ? how_it_works_data.how_it_works_en.section_heading_second
+                : how_it_works_data.how_it_works_fr.section_heading_second}
+            </p>
+          </div>
+        </div>
+
+        <div className="row align-items-center">
+          <div className="col-sm-12 col-md-8">
+            <p>
+              {lang === "en"
+                ? how_it_works_data.how_it_works_en.section_heading_third
+                : how_it_works_data.how_it_works_fr.section_heading_third}
+            </p>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <img
+              src="./assets/image_sample.png"
+              width={220}
+              height={150}
+              alt="config"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default HomeItWorks;
---
+++ src/resources/how-it-works_data.js
@@ -0,0 +1,24 @@
+// Textual Data for How it Works component
+const how_it_works_data = {
+  how_it_works_en: {
+    title: "How it works?",
+    section_heading_first:
+      "Non-profit organizations working with kids in underprivileged and often neglected parts of the world list artwork created by children on the Helkidgro platform.",
+    section_heading_second:
+      "Supporters can browse artwork and also read a short story about the child who created that piece of art, because art is not just about what you see, its also about the story behind that masterpiece.",
+    section_heading_third:
+      "If you find an artwork you like, you could support the kid by either donating, or by ordering an item of your choice (mug, t-shirt and more) using that artwork. All proceeds from sale goes to the non-profit which will than utilize the funds on grassroots level.",
+  },
+  // above in french language
+  how_it_works_fr: {
+    title: "Comment ça marche?",
+    section_heading_first:
+      "Les organisations à but non lucratif travaillant avec des enfants dans des régions défavorisées et souvent négligées du monde entier répertorient les œuvres d'art créées par des enfants sur la plate-forme Helkidgro.",
+    section_heading_second:
+      "Les supporters peuvent parcourir les œuvres d'art et également lire une courte histoire sur l'enfant qui a créé cette œuvre d'art, car l'art ne concerne pas seulement ce que vous voyez, c'est aussi l'histoire derrière ce chef-d'œuvre.",
+    section_heading_third:
+      "Si vous trouvez une œuvre d'art que vous aimez, vous pouvez soutenir l'enfant en faisant un don ou en commandant un article de votre choix (tasse, t-shirt et plus) en utilisant cette œuvre d'art. Tous les bénéfices de la vente vont à l'organisme sans but lucratif qui utilisera ensuite les fonds au niveau de la base.",
+  },
+};
+
+export default how_it_works_data;
